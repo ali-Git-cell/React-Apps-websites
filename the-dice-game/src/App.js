4e159758461cbbe51d0d02f3d1843e7fba265e6c
Ali A. Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2021-12-28 11:57:24 +0100

    Add files via upload

---
+++ the-dice-game/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Rdice from "./Rdice";
+
+function App() {
+  return (
+    <div className="App">
+      <Rdice />
+    </div>
+  );
+}
+
+export default App;
---
+++ the-dice-game/src/Die.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+export default class Die extends Component {
+  render() {
+    return <i className={`Die fas fa-dice-${this.props.face}`} />;
+  }
+}
