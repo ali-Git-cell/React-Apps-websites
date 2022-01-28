8fdf27cc4362260df83b94c0327aa1a143319eb2
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2022-01-28 17:18:26 +0100

    Delete coin-game directory

--- coin-game/src/Coin.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Coin extends Component {
-    render() {
-        return (
-            <img src={this.props.info.imgSrc} alt={this.props.info.side}/>
-        )
-    }
-}
